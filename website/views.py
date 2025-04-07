d71ae75326cbc4b10dfb920e9468c8c64fade9cc
Piotr Kadula <ppracakadula@gmail.com>
2025-04-07 20:12:14 +0200

    add, update, delete records

--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
-
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +20,7 @@ def home(request):
             messages.error(request, "There Was An Error Logging In, Please Try Again")
             return redirect('home')
 
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'records':records})
 
 def logout_user(request):
     logout(request)
@@ -43,3 +44,50 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
     
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.error(request, "You Must Be Logged In To View This Page")
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        # Look up records
+        delete_it = Record.objects.get(id=pk)
+        # Delete record
+        delete_it.delete()
+        messages.success(request, "You Have Successfully Deleted Record")
+        return redirect('home')
+    else:
+        messages.error(request, "You Must Be Logged In To View This Page")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "You Have Successfully Added Record")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.error(request, "You Must Be Logged In To Add Record")
+        return redirect('home')
+
+def update_record(request, pk):
+        if request.user.is_authenticated:
+            current_record = Record.objects.get(id=pk)
+            form = AddRecordForm(request.POST or None, instance=current_record)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "You Have Successfully Updated Record")
+                return redirect('home')
+            return render(request, 'update_record.html', {'form':form})
+        else:
+            messages.error(request, "You Must Be Logged In To Update Record")
+
